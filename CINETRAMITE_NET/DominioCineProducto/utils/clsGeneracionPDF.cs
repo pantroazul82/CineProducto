162a162bc1b1f7098304d6d784c6d25637b76ec3
John Avila <javilaeg@mincultura.gov.co>
2023-10-11 18:03:47 -0500

    ajustes en el formulario de firma y en el listado de las obras

--- CINETRAMITE_NET/DominioCineProducto/utils/clsGeneracionPDF.cs
+++ CINETRAMITE_NET/DominioCineProducto/utils/clsGeneracionPDF.cs
@@ -38,54 +38,54 @@ namespace DominioCineProducto.utils
 
         public void generarFormularioProducto(HttpServerUtility s, System.Web.UI.Page pagina, List<parametroRemplazo> lista)
         {
-            //generarPdf(s, pagina, "Formulario_Producto_2023_V2.docx", lista);
+            generarPdf(s, pagina, "Formulario_Producto_2023_V2.docx", lista);
         }
 
 
-        //public void generarPdf(HttpServerUtility s, System.Web.UI.Page pagina, string plantilla, List<parametroRemplazo> listaRemplazar)
-        //{
-        //    System.Configuration.AppSettingsReader ar = new System.Configuration.AppSettingsReader();
-        //    string ruta = s.MapPath(rutaPlantillas + "/" + plantilla);
-        //    Xceed.Document.NET.Document d = DocX.Load(ruta);
+        public void generarPdf(HttpServerUtility s, System.Web.UI.Page pagina, string plantilla, List<parametroRemplazo> listaRemplazar)
+        {
+            System.Configuration.AppSettingsReader ar = new System.Configuration.AppSettingsReader();
+            string ruta = s.MapPath(rutaPlantillas + "/" + plantilla);
+            Xceed.Document.NET.Document d = DocX.Load(ruta);
 
-        //    Xceed.Document.NET.Formatting fBold = new Xceed.Document.NET.Formatting();
-        //    fBold.Bold = true;
-        //    fBold.Size = 10;
-        //    Xceed.Document.NET.Formatting fNormal = new Xceed.Document.NET.Formatting();
-        //    fNormal.Bold = false;
-        //    fNormal.Size = 8;
-        //    string pathArchivosTemp = ar.GetValue("pathArchivosTemp", typeof(string)).ToString();
-        //    string pathArchivosTempRelativo = ar.GetValue("pathArchivosTemp", typeof(string)).ToString(); ;
-        //    if (listaRemplazar != null)
-        //    {
-        //        foreach (var item in listaRemplazar)
-        //        {
-        //            d.ReplaceText(item.parametro, item.remplazo, false);
-        //        }
-        //    }
+            Xceed.Document.NET.Formatting fBold = new Xceed.Document.NET.Formatting();
+            fBold.Bold = true;
+            fBold.Size = 10;
+            Xceed.Document.NET.Formatting fNormal = new Xceed.Document.NET.Formatting();
+            fNormal.Bold = false;
+            fNormal.Size = 8;
+            string pathArchivosTemp = ar.GetValue("pathArchivosTempFormulario", typeof(string)).ToString();
+            string pathArchivosTempRelativo = ar.GetValue("pathArchivosTemp", typeof(string)).ToString(); ;
+            if (listaRemplazar != null)
+            {
+                foreach (var item in listaRemplazar)
+                {
+                    d.ReplaceText(item.parametro, item.remplazo, false);
+                }
+            }
 
-        //    //d.Save();
-        //    string nombreFinal = DateTime.Now.Ticks.ToString().Substring(10) + "cer_Inversion.docx";
-        //    string rutaFinal = pathArchivosTemp + @"\" + nombreFinal;
-        //    d.SaveAs(rutaFinal);
-        //    //el save as de arriba deberia ir a una ruta temporal
-        //    RichEditDocumentServer server = new RichEditDocumentServer();
-        //    server.LoadDocument(rutaFinal);
-        //    //
-        //    PdfExportOptions options = new PdfExportOptions();
-        //    options.DocumentOptions.Author = "Ministerio de Cultura";
-        //    options.Compressed = true;
-        //    options.ImageQuality = PdfJpegImageQuality.Highest;
-        //    //Export the document to the stream: 
-        //    using (FileStream pdfFileStream = new FileStream(rutaFinal.Replace(".docx", ".pdf"), FileMode.Create))
-        //    {
-        //        server.ExportToPdf(pdfFileStream, options);
-        //    }
+            //d.Save();
+            string nombreFinal = DateTime.Now.Ticks.ToString().Substring(10) + "cer_Inversion.docx";
+            string rutaFinal = pathArchivosTemp + @"\" + nombreFinal;
+            d.SaveAs(rutaFinal);
+            //el save as de arriba deberia ir a una ruta temporal
+            RichEditDocumentServer server = new RichEditDocumentServer();
+            server.LoadDocument(rutaFinal);
+            //
+            PdfExportOptions options = new PdfExportOptions();
+            options.DocumentOptions.Author = "Ministerio de Cultura";
+            options.Compressed = true;
+            options.ImageQuality = PdfJpegImageQuality.Highest;
+            //Export the document to the stream: 
+            using (FileStream pdfFileStream = new FileStream(rutaFinal.Replace(".docx", ".pdf"), FileMode.Create))
+            {
+                server.ExportToPdf(pdfFileStream, options);
+            }
 
-        //    pagina.ClientScript.RegisterStartupScript(
-        //         this.GetType(), "OpenWindow",
-        //         "window.open('" + "../../" + pathArchivosTempRelativo.Replace("~/", "") + "/" + nombreFinal.Replace(".docx", ".pdf") + "','_newtab" + DateTime.Now.Ticks + "');", true);
+            pagina.ClientScript.RegisterStartupScript(
+                 this.GetType(), "OpenWindow",
+                 "window.open('" + "../../" + pathArchivosTempRelativo.Replace("~/", "") + "/" + nombreFinal.Replace(".docx", ".pdf") + "','_newtab" + DateTime.Now.Ticks + "');", true);
 
-        //}
+        }
     }
 }
